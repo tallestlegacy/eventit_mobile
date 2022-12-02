24e8b2de9237acbec502a7f3873ff03dc791f8a5
Marvin <marvinmokua@gmail.com>
2022-12-02 14:43:24 +0300

    init

---
+++ android/app/src/main/kotlin/com/example/eventit_mobile/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.eventit_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import "package:flutter/material.dart";
+import 'package:flutterfire_ui/auth.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: AuthGate(),
+    );
+  }
+}
+
+class AuthGate extends StatelessWidget {
+  const AuthGate({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        // User is not signed in
+        if (!snapshot.hasData) {
+          return const SignInScreen(providerConfigs: []);
+        }
+
+        // Render your application if authenticated
+        return const Home();
+      },
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Hello world"),
+      ),
+    );
+  }
+}
